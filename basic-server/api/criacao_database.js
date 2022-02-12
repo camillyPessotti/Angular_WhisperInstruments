3de7558b1d373f809de85a41a3f67da0c9c9f40c
camillyPessotti <camilly_pessotti@estudante.sc.senai.br>
2022-02-11 21:08:20 -0300

    commit

--- basic-server/api/criacao_database.js
+++ basic-server/api/criacao_database.js
@@ -1,24 +1,22 @@
-database(`CREATE TABLE IF NOT EXISTS TESTE (
-    ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    NOME varchar(30),
-    NUMERO int
-    )`)
+database(`CREATE TABLE IF NOT EXISTS PESSOA (
+        CPF CHAR(11) PRIMARY KEY,
+        NOME VARCHAR(45) NOT NULL,
+        IDADE INT)`)
     .then(result => {
-        console.log('TABELA CRIADA COM SUCESSO');
-    })
-    .catch(erro => {
-        console.log('ERRO AO CRIAR A TABELA');
-        console.log(erro)
+        console.log('TABELA PESSOA CRIADA COM SUCESSO');
+    }).catch(erro => {
+        console.log('ERRO AO CRIAR A TABELA PESSOA');
     });
 
-database(`CREATE TABLE IF NOT EXISTS USERS (
-        ID INTEGER PRIMARY KEY AUTOINCREMENT,
-        NOME varchar(30),
-        NICKNAME varchar(30)
-        )`)
+database(`CREATE TABLE IF NOT EXISTS CLIENTE (
+    CODIGO INT AUTO_INCREMENT PRIMARY KEY,
+    EMAIL VARCHAR(100),
+    USERNAME VARCHAR(45) NOT NULL, 
+    PASSWORD VARCHAR(15) NOT NULL,
+    PESSOA_CPF CHAR(11) NOT NULL,
+    FOREIGN KEY (PESSOA_CPF) REFERENCES PESSOA(CPF))`)
     .then(result => {
-        console.log('TABELA CRIADA COM SUCESSO');
-    })
-    .catch(erro => {
-        console.log('ERRO AO CRIAR A TABELA');
+        console.log('TABELA CLIENTE CRIADA COM SUCESSO');
+    }).catch(erro => {
+        console.log('ERRO AO CRIAR A TABELA CLIENTE');
     });
