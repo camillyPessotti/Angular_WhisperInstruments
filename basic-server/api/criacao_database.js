a75a20e5d521eecdb56abbd4ca521b64243b51a1
camillyPessotti <camilly_pessotti@estudante.sc.senai.br>
2022-02-21 21:38:19 -0300

    commit

--- basic-server/api/criacao_database.js
+++ basic-server/api/criacao_database.js
@@ -65,52 +65,52 @@ database(`CREATE TABLE IF NOT EXISTS PRODUTO (
         console.log('ERRO ao criar a tabela PRODUTO');
     });
 
-database(`INSERT INTO ENDERECO VALUES 
-(null, 'Olga Erdmann', 'Barra do Rio Cerro', 'Jaraguá do Sul', 'Santa Catarina'),
-(null, 'Afonso Beijamin Barbie', 'Tifa Martins', 'Jaraguá do Sul', 'Santa Catarina'),
-(null, 'Albano Piccoli', 'São Luís', 'Jaraguá do Sul', 'Santa Catarina'),
-(null, 'Rua das Orquídeas', 'Jaraguá 99', 'Jaraguá do Sul', 'Santa Catarina'),
-(null, 'José Narloch', 'Tifa Martins', 'Jaraguá do Sul', 'Santa Catarina')`)
-    .then(result => {
-        console.log('Dados inseridos com SUCESSO na tabela ENDERECO');
-    }).catch(erro => {
-        console.log('ERRO ao inserir dados na tabela ENDERECO');
-    });
+// database(`INSERT INTO ENDERECO VALUES 
+// (null, 'Olga Erdmann', 'Barra do Rio Cerro', 'Jaraguá do Sul', 'Santa Catarina'),
+// (null, 'Afonso Beijamin Barbie', 'Tifa Martins', 'Jaraguá do Sul', 'Santa Catarina'),
+// (null, 'Albano Piccoli', 'São Luís', 'Jaraguá do Sul', 'Santa Catarina'),
+// (null, 'Rua das Orquídeas', 'Jaraguá 99', 'Jaraguá do Sul', 'Santa Catarina'),
+// (null, 'José Narloch', 'Tifa Martins', 'Jaraguá do Sul', 'Santa Catarina')`)
+//     .then(result => {
+//         console.log('Dados inseridos com SUCESSO na tabela ENDERECO');
+//     }).catch(erro => {
+//         console.log('ERRO ao inserir dados na tabela ENDERECO');
+//     });
 
 
-database(`INSERT INTO PESSOA VALUES 
-('11111111111', 'Camilly Pessotti', 17, 1),
-('22222222222', 'João Meireles', 17, 2),
-('33333333333', 'Diego Planinscheck', 17, 3),
-('44444444444', 'Bruna Mafra', 17, 4),
-('55555555555', 'Leonardo Rafaelli', 17, 5)`)
-    .then(result => {
-        console.log('Dados inseridos com SUCESSO na tabela PESSOA');
-    }).catch(erro => {
-        console.log('ERRO ao inserir dados na tabela PESSOA');
-    });
+// database(`INSERT INTO PESSOA VALUES 
+// ('11111111111', 'Camilly Pessotti', 17, 1),
+// ('22222222222', 'João Meireles', 17, 2),
+// ('33333333333', 'Diego Planinscheck', 17, 3),
+// ('44444444444', 'Bruna Mafra', 17, 4),
+// ('55555555555', 'Leonardo Rafaelli', 17, 5)`)
+//     .then(result => {
+//         console.log('Dados inseridos com SUCESSO na tabela PESSOA');
+//     }).catch(erro => {
+//         console.log('ERRO ao inserir dados na tabela PESSOA');
+//     });
 
 
-database(`INSERT INTO CLIENTE VALUES 
-(null, 'camillypessotti@gmail.com', 'camillyPessotti', '12345', '11111111111'),
-(null, 'joaomeireles@gmail.com', 'joaoMeireles', '12222', '22222222222'),
-(null, 'diegoplaninscheck@gmail.com', 'diegoPlaninscheck', '13333', '33333333333'),
-(null, 'brunamafra@gmail.com', 'brunaMafra', '14444', '44444444444'),
-(null, 'leonardorafaelli@gmail.com', 'leonardoRafaelli', '15555', '55555555555')`)
-    .then(result => {
-        console.log('Dados inseridos com SUCESSO na tabela CLIENTE');
-    }).catch(erro => {
-        console.log('ERRO ao inserir dados na tabela CLIENTE');
-    });
+// database(`INSERT INTO CLIENTE VALUES 
+// (null, 'camillypessotti@gmail.com', 'camillyPessotti', '12345', '11111111111'),
+// (null, 'joaomeireles@gmail.com', 'joaoMeireles', '12222', '22222222222'),
+// (null, 'diegoplaninscheck@gmail.com', 'diegoPlaninscheck', '13333', '33333333333'),
+// (null, 'brunamafra@gmail.com', 'brunaMafra', '14444', '44444444444'),
+// (null, 'leonardorafaelli@gmail.com', 'leonardoRafaelli', '15555', '55555555555')`)
+//     .then(result => {
+//         console.log('Dados inseridos com SUCESSO na tabela CLIENTE');
+//     }).catch(erro => {
+//         console.log('ERRO ao inserir dados na tabela CLIENTE');
+//     });
 
 
-database(`INSERT INTO CLASSE VALUES
-(null, 'Instrumento de corda'),
-(null, 'Instrumento de sopro'),
-(null, 'Instrumento de percussão'),
-(null, 'Instrumentos elétricos')`)
-    .then(result => {
-        console.log('Dados inseridos com SUCESSO na tabela CLASSE');
-    }).catch(erro => {
-        console.log('ERRO ao inserir dados na tabela CLASSE');
-    });
+// database(`INSERT INTO CLASSE VALUES
+// (null, 'Instrumento de corda'),
+// (null, 'Instrumento de sopro'),
+// (null, 'Instrumento de percussão'),
+// (null, 'Instrumentos elétricos')`)
+//     .then(result => {
+//         console.log('Dados inseridos com SUCESSO na tabela CLASSE');
+//     }).catch(erro => {
+//         console.log('ERRO ao inserir dados na tabela CLASSE');
+//     });
