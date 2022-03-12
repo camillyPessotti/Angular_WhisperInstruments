732e7a049766ca15802385449f09ae5edb22c8fa
Camilly Pessotti <camilly_pessotti@estudante.sc.senai.br>
2022-03-12 15:15:22 -0300

    commit

---
+++ ultima_atividade_angular/src/app/carrinho/tela-carrinho/tela-carrinho.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-carrinho',
+  templateUrl: './tela-carrinho.component.html',
+  styleUrls: ['./tela-carrinho.component.css']
+})
+export class TelaCarrinhoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
