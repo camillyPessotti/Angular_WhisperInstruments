d99e1dc34e5beebdbeccef26ea13c2acf26083b6
camillyPessotti <camilly_pessotti@estudante.sc.senai.br>
2022-03-21 20:56:15 -0300

    commit

---
+++ ultima_atividade_angular/src/app/produto/produto.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TelaProdutoComponent } from './tela-produto/tela-produto.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TelaProdutoComponent]
+})
+export class ProdutoModule { }
