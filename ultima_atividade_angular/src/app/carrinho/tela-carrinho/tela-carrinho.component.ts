21cd50ebbb52e6e5810c65f1322545a937fbac47
camillyPessotti <camilly_pessotti@estudante.sc.senai.br>
2022-03-21 21:05:59 -0300

    commit

--- ultima_atividade_angular/src/app/carrinho/tela-carrinho/tela-carrinho.component.ts
+++ ultima_atividade_angular/src/app/carrinho/tela-carrinho/tela-carrinho.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tela-carrinho',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TelaCarrinhoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
 
   ngOnInit() {
+  }
+
+  irProLogin(){
+    this.router.navigate(['/tela-login'])
+  }
+
+  voltar(){
+    this.router.navigate([''])
   }
 
 }
